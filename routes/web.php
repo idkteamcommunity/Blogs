b08d0393657cef2612f8e2ef5013e10134ea8ecc
fadhluibnu <f.ibnu.a1@gmail.com>
2021-12-24 10:12:15 +0700

    Web.php : Route Home

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home', [
+        'title' => 'IDK TEAM BLOGS',
+        'active' => 'home'
+    ]);
 });
