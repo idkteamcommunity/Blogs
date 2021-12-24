31c2f1018a2aef0eb80721528ce532c1c54cbb69
fadhluibnu <f.ibnu.a1@gmail.com>
2021-12-24 10:14:30 +0700

    Resource View Navbar

---
+++ resources/views/navbar/navbar.blade.php
@@ -0,0 +1,61 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-white">
+    <div class="container-fluid">
+        @include('navbar.offcanvas')
+        <a class="navbar-brand d-flex align-items-center ms-2 me-md-auto" href="#">
+            <img src="Assets/logo.svg" alt="blogs logo" width="40" class="d-inline-block align-text-top">
+            <span class="my-logo ms-2">IDK</span>
+        </a>
+        <div class="collapse navbar-collapse d-none d-lg-flex ms-3" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link {{ $active === 'home' ? 'active' : '' }}" href="/"><i
+                            class="bi bi-house-door-fill"></i>
+                        Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link {{ $active === 'blog' ? 'active' : '' }}" href="/blog"><i
+                            class="bi bi-blockquote-left"></i> Blogs</a>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle {{ $active === 'category' ? 'active' : '' }}" href="/category"
+                        id="category" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        <i class="bi bi-x-diamond-fill"></i> Category
+                    </a>
+                    <ul class="dropdown-menu" aria-labelledby="category">
+                        <li><a class="dropdown-item" href="#">Action</a></li>
+                        <li><a class="dropdown-item" href="#">Another action</a></li>
+                        <li>
+                            <hr class="dropdown-divider">
+                        </li>
+                        <li><a class="dropdown-item" href="#">Something else here</a></li>
+                    </ul>
+                </li>
+            </ul>
+        </div>
+        <form class="d-none d-sm-flex">
+            <div class="input-group">
+                <input type="text" class="form-control" placeholder="Temukan postingan">
+                <button class="btn btn-idk px-4 py-2" type="submit" id="button-addon2"><i
+                        class="bi bi-search"></i></button>
+            </div>
+        </form>
+        <button class="btn btn-idk px-4 py-2 d-flex d-sm-none" type="button" data-bs-toggle="offcanvas"
+            data-bs-target="#offcanvasTop" aria-controls="offcanvasTop"><i class="bi bi-search"></i></button>
+        <div class="offcanvas offcanvas-top" tabindex="-1" id="offcanvasTop" aria-labelledby="offcanvasTopLabel">
+            <div class="offcanvas-header">
+                <h5 id="offcanvasTopLabel">Offcanvas top</h5>
+                <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas"
+                    aria-label="Close"></button>
+            </div>
+            <div class="offcanvas-body">
+                <form class="d-flex">
+                    <div class="input-group">
+                        <input type="text" class="form-control" placeholder="Temukan postingan">
+                        <button class="btn btn-idk px-4 py-2" type="submit" id="button-addon2"><i
+                                class="bi bi-search"></i></button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</nav>
---
+++ resources/views/navbar/offcanvas.blade.php
@@ -0,0 +1,39 @@
+<a class="navbar-toggler" data-bs-toggle="offcanvas" href="#offcanvasExample" role="button"
+    aria-controls="offcanvasExample">
+    <span class="navbar-toggler-icon"></span>
+</a>
+<div class="offcanvas offcanvas-start" tabindex="-1" id="offcanvasExample" aria-labelledby="offcanvasExampleLabel">
+    <div class="offcanvas-header">
+        <h5 class="offcanvas-title" id="offcanvasExampleLabel">
+            <a class="navbar-brand d-flex align-items-center" href="#">
+                <img src="Assets/logo.svg" alt="blogs logo" width="40" class="d-inline-block align-text-top">
+                <span class="my-logo ms-2">IDK</span>
+            </a>
+        </h5>
+        <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+    </div>
+    <ul class="navbar-nav px-4">
+        <li class="nav-item">
+            <a class="nav-link {{ $active === 'home' ? 'active' : '' }}" href="/"><i class="bi bi-house-door-fill"></i>
+                Home</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link {{ $active === 'blog' ? 'active' : '' }}" href="/blog"><i
+                    class="bi bi-blockquote-left"></i> Blogs</a>
+        </li>
+        <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle {{ $active === 'category' ? 'active' : '' }}" href="/category"
+                id="category" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                <i class="bi bi-x-diamond-fill"></i> Category
+            </a>
+            <ul class="dropdown-menu" aria-labelledby="category">
+                <li><a class="dropdown-item" href="#">Action</a></li>
+                <li><a class="dropdown-item" href="#">Another action</a></li>
+                <li>
+                    <hr class="dropdown-divider">
+                </li>
+                <li><a class="dropdown-item" href="#">Something else here</a></li>
+            </ul>
+        </li>
+    </ul>
+</div>
