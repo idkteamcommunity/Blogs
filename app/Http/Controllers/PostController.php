85794bbd5f32d79759c00ddd118b586a18262c76
fadhluibnu <f.ibnu.a1@gmail.com>
2021-12-29 10:52:18 +0700

    tambah function index untuk melakuakan pencarian dan menampilkan positingan & function show untuk single post page

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,9 +2,36 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
-    //
+
+    public function index()
+    {
+        $post = Post::latest();
+        if (request('urutan')) {
+            $post = Post::orderBy('id', request('urutan'));
+        }
+
+        return view('posts', [
+            'title' => 'Postingan',
+            'active' => 'posts',
+            'categories' => Category::all(),
+            'posts' => $post->filter(request(['search', 'kategori']))->get()
+        ]);
+    }
+
+    public function show(Post $post)
+    {
+        return view('singlePost', [
+            'title' =>  "$post->title - IDK Team",
+            'active' => 'single',
+            'posts' => $post,
+            // 'related' => $post->category,
+            'postRealted' => Post::all()
+        ]);
+    }
 }
