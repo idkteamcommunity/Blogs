985bc9c0bff3bdbeff1408da4e0e2c815e06a58b
fadhluibnu <f.ibnu.a1@gmail.com>
2021-12-29 14:13:56 +0700

    Menambahakan Seeder post, user, category

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\Category;
+use App\Models\Post;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,5 +17,21 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+        Post::factory(8)->create();
+
+        User::factory(3)->create();
+
+        Category::create([
+            'name' => 'Programming',
+            'slug' => 'programming'
+        ]);
+        Category::create([
+            'name' => 'Web Design',
+            'slug' => 'web-design'
+        ]);
+        Category::create([
+            'name' => 'Hacking',
+            'slug' => 'hacking'
+        ]);
     }
 }
